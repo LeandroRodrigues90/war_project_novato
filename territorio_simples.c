d2961f418fe70e90df716075160bc1ab043d0d48
LeandroRodrigues90 <leandrorodriguesdasilvalima@gmail.com>
2025-09-01 20:15:50 -0300

    algumas atualizações e refatoração

--- territorio_simples.c
+++ territorio_simples.c
@@ -1,21 +1,23 @@
+// Inclusão das bibliotecas padrão necessárias para entrada/saída, alocação de memória, manipulação de strings e tempo.
 #include <stdio.h>
 #include <string.h>
 
-// Inclusão das bibliotecas padrão necessárias para entrada/saída, alocação de memória, manipulação de strings e tempo.
+
+
+// --- Constantes Globais ---
+// Definem valores fixos para o número de territórios, missões e tamanho máximo de strings, facilitando a manutenção.
 #define MAX_TROPAS 5
 #define TAM_STRING_NOME 30
 #define TAM_STRING_COR 10
-// --- Constantes Globais ---
-// Definem valores fixos para o número de territórios, missões e tamanho máximo de strings, facilitando a manutenção.
+
+// --- Estrutura de Dados ---
+// Define a estrutura para um território, contendo seu nome, a cor do exército que o domina e o número de tropas.
 struct Territorio
 {
     char nome[TAM_STRING_NOME];
     char cor[TAM_STRING_COR];
     int tropas;
 };
-
-// --- Estrutura de Dados ---
-// Define a estrutura para um território, contendo seu nome, a cor do exército que o domina e o número de tropas.
 
 // --- Protótipos das Funções ---
 void limparBufferEntrada(){
@@ -35,7 +37,8 @@ void limparBufferEntrada(){
 int main() {
 
     struct Territorio combat[MAX_TROPAS];
-    
+    int opcaoAtaque;
+    int opcaoDefesa;
 
     printf("Vamos cadastrar 5 territorios iniciais do nosso mundo.\n\n");
 
@@ -46,7 +49,7 @@ int main() {
             printf("Nome do Territorio: ");
             fgets(combat[i].nome,TAM_STRING_NOME,stdin);
 
-            printf("Cor do Territorio:(Azul,Verde...) ");
+            printf("Cor do Territorio (Ex:Azul,Verde...): ");
             fgets(combat[i].cor,TAM_STRING_COR,stdin);
 
             combat[i].nome[strcspn(combat[i].nome,"\n")]=0;
@@ -59,18 +62,31 @@ int main() {
             printf("\n");
            
         }
-        printf("Relatório de Territorios cadastrados\n");
-        printf("--------------------------------\n");
+        do
+        {
+            printf("====================================\n");
+            printf("      Mapa do mundo-Estado atual    \n");
+            printf("=====================================\n");
         for (int i=0;i<MAX_TROPAS;i++){
 
-            printf("\n");
-            printf("Territorio -%d\n",i+1);
-            printf("Nome do Territorio: %s\n",combat[i].nome);
-            printf("Cor do Territorio: %s\n",combat[i].cor);
-            printf("Tropas: %d\n",combat[i].tropas);
-            printf("\n");
-            printf("---------------------------------\n\n");
+            printf("%d- %s (Exercito: %s, Tropas:%d)\n",i+1,combat[i].nome,combat[i].cor,combat[i].tropas);
+            
         }
+            printf("\n            Fase de Ataque           \n");
+            printf("----------------------------------------\n");
+            printf("Escolha o Territorio do Atacante (1 a 5 ou 0 para sair): ");
+            scanf("%d",&opcaoAtaque);
+            
+            if(opcaoAtaque>=1 || opcaoAtaque<=5){
+                printf("Escolha o Territorio para defender(1 a 5): ");
+                scanf("%d",&opcaoDefesa);
+            }else{
+                break;
+            }
+
+        } while (opcaoAtaque != 0);
+            printf("Finalizando jogo...");
+
 
         return 0;
     }
